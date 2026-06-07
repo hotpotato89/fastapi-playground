80508a84003ca29d755694c41e17e71edb2cf516
hotpotato <ramilreading@gmail.com>
2026-06-07 14:39:42 +0300

    feat: added book model

---
+++ src/app/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, TIMESTAMP, Text, func
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    title: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
+    author: Mapped[str] = mapped_column(String(60), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text(), nullable=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), server_default=func.now()
+    )
