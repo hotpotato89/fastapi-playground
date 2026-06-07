23888d89aae3e480bad893d0a4987550fc707204
hotpotato <ramilreading@gmail.com>
2026-06-07 15:14:16 +0300

    feat: 307 -> 301

--- src/app/routers/root.py
+++ src/app/routers/root.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from fastapi.responses import RedirectResponse
 
 router = APIRouter(tags=["root"])
@@ -6,4 +6,4 @@ router = APIRouter(tags=["root"])
 
 @router.get("/")
 async def root():
-    return RedirectResponse("/docs")
+    return RedirectResponse("/docs", status_code=status.HTTP_301_MOVED_PERMANENTLY)
