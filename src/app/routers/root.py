031ac932cb30e555e38e641b749dcbac6e13695e
hotpotato <ramilreading@gmail.com>
2026-06-08 18:21:36 +0300

    docs: added -> RedirectResponse

--- src/app/routers/root.py
+++ src/app/routers/root.py
@@ -5,5 +5,5 @@ router = APIRouter(tags=["root"])
 
 
 @router.get("/")
-async def root():
+async def root() -> RedirectResponse:
     return RedirectResponse("/docs", status_code=status.HTTP_301_MOVED_PERMANENTLY)
