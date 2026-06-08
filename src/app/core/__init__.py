1ffd4ef36ed8aa911adf211063dc2aa34f95ebe8
hotpotato <ramilreading@gmail.com>
2026-06-08 18:22:51 +0300

    refactor: renamed config -> settings

--- src/app/core/__init__.py
+++ src/app/core/__init__.py
@@ -1,4 +1,4 @@
-from src.app.core.config import settings
+from src.app.core.settings import settings
 from src.app.core.database import engine, SessionLocal
 from src.app.core.lifespan import lifespan
 
