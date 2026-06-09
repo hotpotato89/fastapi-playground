a08aff06d6ed5db97d2c745ba5ac246bdffbf4ce
hotpotato <ramilreading@gmail.com>
2026-06-09 17:28:22 +0300

    feat: added update schema

--- src/app/schemas/book_schemas.py
+++ src/app/schemas/book_schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from .base import BaseSchema
 from pydantic import Field, field_validator
@@ -7,7 +8,7 @@ from pydantic import Field, field_validator
 class BookCreate(BaseSchema):
     title: str = Field(..., max_length=30, description="Book title")
     author: str = Field(..., max_length=60, description="Book author")
-    description: str | None = Field(None, description="Book desctiption")
+    description: Optional[str] = Field(None, description="Book desctiption")
 
     @field_validator("title", "author")
     @classmethod
@@ -23,5 +24,20 @@ class BookResponse(BaseSchema):
     id: int
     title: str
     author: str
-    description: str | None = None
+    description: Optional[str] = None
     created_at: datetime
+
+
+class BookUpdate(BaseSchema):
+    title: Optional[str] = Field(None, max_length=30, description="Book title")
+    author: Optional[str] = Field(None, max_length=60, description="Book author")
+    description: Optional[str] = Field(None, description="Book description")
+
+    @field_validator("title", "author")
+    @classmethod
+    def not_empty(cls, v: Optional[str]):
+        if v is not None:
+            if not v.strip():
+                raise ValueError("Cannot be empty")
+            return v.strip().capitalize()
+        return v
