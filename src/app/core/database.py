799e924963aa691884e2add7b4aec84fc1f2fdc7
hotpotato <ramilreading@gmail.com>
2026-06-09 11:02:46 +0300

    feat: added some database settings

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -2,7 +2,11 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from src.app.core import settings
 
 engine = create_async_engine(
-    settings.db.url, pool_size=10, max_overflow=15, pool_pre_ping=True, pool_recycle=60*60
+    settings.db.url,
+    pool_size=10,
+    max_overflow=15,
+    pool_pre_ping=True,
+    pool_recycle=60 * 60,
 )
 
 SessionLocal = async_sessionmaker(
