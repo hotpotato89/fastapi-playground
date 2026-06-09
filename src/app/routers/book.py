bfc78fe8192f8cea6debe6e9591c602ca681511b
hotpotato <ramilreading@gmail.com>
2026-06-09 11:07:54 +0300

    docs: added 201 syayus code on create_book endpoint

--- src/app/routers/book.py
+++ src/app/routers/book.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Path, Query
+from fastapi import APIRouter, Depends, Path, Query, status
 from src.app.routers.deps import get_service
 from src.app.schemas.book_schemas import BookCreate, BookResponse
 from src.app.services.book_service import BookService
@@ -8,7 +8,7 @@ from src.app.services.book_service import BookService
 router = APIRouter(tags=["book"], prefix="/book")
 
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(
     service: Annotated[BookService, Depends(get_service)], upload_data: BookCreate
 ) -> BookResponse:
