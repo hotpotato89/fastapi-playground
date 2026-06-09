030b1dea04ed9bd6e3fd51e18ad18b8bb4e6deee
hotpotato <ramilreading@gmail.com>
2026-06-09 19:23:12 +0300

    feat: added users table

--- src/app/models.py
+++ src/app/models.py
@@ -22,3 +22,16 @@ class Book(Base):
     )
 
     genre: Mapped[str] = mapped_column(String(50), nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), server_default=func.now()
+    )
