fe8e252c4773f629a7c09523a834a9d094d1a201
hotpotato <ramilreading@gmail.com>
2026-06-10 20:54:27 +0300

    added CORS

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.exception_handlers import register_exception_handlers
 from src.app.routers import root_router
@@ -7,6 +8,13 @@ from src.app.main_router import main_router
 
 app = FastAPI(title="FastAPI playground", lifespan=lifespan)
 register_exception_handlers(app)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(root_router)
 app.include_router(main_router)
