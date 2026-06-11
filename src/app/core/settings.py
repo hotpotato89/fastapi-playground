9c18019d88dfddf58afd07cefdb8c2a4f709af5f
hotpotato <ramilreading@gmail.com>
2026-06-11 15:47:04 +0300

    feat: added configable cors

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -14,14 +14,22 @@ class DBSettings(BaseModel):
         return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
 
 
-class JWTSetings(BaseModel):
+class JWTSettings(BaseModel):
     secret_key: str
     algorithm: str = "HS256"
 
 
+class DeploySettings(BaseModel):
+    allow_origins: list[str] = ["*"]
+    allow_credentials: bool = False
+    allow_methods: list[str] = ["*"]
+    allow_headers: list[str] = ["Content-Type", "Authorization"]
+
+
 class Settings(BaseSettings):
     db: DBSettings
-    jwt: JWTSetings
+    jwt: JWTSettings
+    deploy: DeploySettings
 
     model_config = {"env_file": ".env", "env_nested_delimiter": "__"}
 
--- src/app/main.py
+++ src/app/main.py
@@ -3,17 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.app.exception_handlers import register_exception_handlers
 from src.app.routers import root_router
-from src.app.core import lifespan
+from src.app.core import lifespan, settings
 from src.app.main_router import main_router
 
 app = FastAPI(title="FastAPI playground", lifespan=lifespan)
 register_exception_handlers(app)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=settings.deploy.allow_origins,
+    allow_credentials=settings.deploy.allow_credentials,
+    allow_headers=settings.deploy.allow_headers,
+    allow_methods=settings.deploy.allow_methods,
 )
 
 app.include_router(root_router)
